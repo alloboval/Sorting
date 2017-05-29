89d822efd2055f602a9095dcd2948123d218591a
alloboval <perfeo@tutanota.com>
2017-05-29 11:26:10 +0300

    Update SelectionSort.cs

--- Sorting/Sorting/Alg/SelectionSort.cs
+++ Sorting/Sorting/Alg/SelectionSort.cs
@@ -9,13 +9,11 @@
             for(int i = 0; i < values.Length - 1; i++)
             {
                 int min = i;
+                
                 for(int j = i + 1; j < values.Length; j++)
-                    {
                         if (flag ? (values[j] < values[min]) : (values[j] > values[min]))
-                        {
                             min = j;
-                        }
-                    }
+                
                 int buff = values[i];
                 values[i] = values[min];
                 values[min] = buff;
